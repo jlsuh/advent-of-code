082755217b395b6ce69006fa9b1cc6390275ea69
jsuh <jsuh@hexacta.com>
2023-12-01 06:55:56 -0300

    refactor: d01

--- 2023/d01/solution.js
+++ 2023/d01/solution.js
@@ -2,16 +2,31 @@ import { readInput } from "../utils.js";
 
 const input = await readInput();
 const start = performance.now();
-const solution1 = input
-  .split("\n")
-  .map((value) => {
-    const digits = value.match(/\d/g);
-    return +(digits[0] + digits[digits.length - 1]);
-  })
-  .reduce((acc, curr) => acc + curr, 0);
-console.log(solution1);
 
+const values = input.split("\n");
+
+const getSum = (values) =>
+  values
+    .map((value) => {
+      const digits = value.match(/\d/g);
+      return +(digits[0] + digits[digits.length - 1]);
+    })
+    .reduce((acc, curr) => acc + curr, 0);
+
+// part-1
+const s1 = getSum(values);
+console.log(s1);
+
+// part-2
 const transform = {
+  oneight: 18,
+  twone: 21,
+  threeight: 38,
+  fiveight: 58,
+  sevenine: 79,
+  eightwo: 82,
+  eighthree: 83,
+  nineight: 98,
   one: 1,
   two: 2,
   three: 3,
@@ -21,29 +36,16 @@ const transform = {
   seven: 7,
   eight: 8,
   nine: 9,
-  oneight: 18,
-  twone: 21,
-  threeight: 38,
-  fiveight: 58,
-  sevenine: 79,
-  eightwo: 82,
-  eighthree: 83,
-  nineight: 98,
 };
 
-const solution2 = input
-  .split("\n")
-  .map((value) => {
-    const val = value.replaceAll(
-      /oneight|twone|threeight|fiveight|sevenine|eightwo|eighthree|nineight|one|two|three|four|five|six|seven|eight|nine/g,
+const s2 = getSum(
+  values.map((value) =>
+    value.replaceAll(
+      new RegExp(Object.keys(transform).join("|"), "g"),
       (matched) => transform[matched],
-    );
-    return val;
-  })
-  .map((value) => {
-    const digits = value.match(/\d/g);
-    return +(digits[0] + digits[digits.length - 1]);
-  })
-  .reduce((acc, curr) => acc + curr, 0);
-console.log(solution2);
+    ),
+  ),
+);
+console.log(s2);
+
 console.log("Elapsed:", performance.now() - start);
