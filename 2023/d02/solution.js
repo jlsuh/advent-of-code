758746fcdcca0c84bfb39d15a222f31d60aab818
jsuh <jsuh@hexacta.com>
2023-12-02 07:52:15 -0300

    feat: d02

---
+++ 2023/d02/solution.js
@@ -0,0 +1,53 @@
+import { readInput } from "../utils.js";
+
+const start = performance.now();
+const input = await readInput();
+
+const games = input
+  .split("\n")
+  .map((game) => game.match("\\d+\\s(.*)"))
+  .map((gameInfo) => gameInfo[0].split("; "))
+  .map((draw) => draw.map((cubes) => cubes.split(", ")));
+
+// part-1
+const max = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+const part1Solution = games.reduce((sum, game, index) => {
+  for (const draws of game) {
+    for (const cubes of draws) {
+      const color = cubes.match("(red|green|blue)")[0];
+      const quantity = cubes.match("\\d+")[0];
+      if (quantity > max[color]) return sum;
+    }
+  }
+  return sum + index + 1;
+}, 0);
+console.log(part1Solution);
+
+// part-2
+let part2Solution = 0;
+for (const game of games) {
+  const fewestCubes = {
+    red: 0,
+    green: 0,
+    blue: 0,
+  };
+  for (const draws of game) {
+    for (const cubes of draws) {
+      const color = cubes.match("(red|green|blue)")[0];
+      const quantity = cubes.match("\\d+")[0];
+      fewestCubes[color] = Math.max(fewestCubes[color], quantity);
+    }
+  }
+  const power = Object.values(fewestCubes).reduce(
+    (prod, curr) => prod * curr,
+    1,
+  );
+  part2Solution += power;
+}
+console.log(part2Solution);
+
+console.log("Elapsed:", performance.now() - start);
