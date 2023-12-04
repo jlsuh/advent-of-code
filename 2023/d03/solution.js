f6e0636b3a1ea81b933644e22c9fb5572230a717
jsuh <jsuh@hexacta.com>
2023-12-04 10:58:18 -0300

    chore: d03

--- 2023/d03/solution.js
+++ 2023/d03/solution.js
@@ -5,12 +5,12 @@ const input = await readInput();
 
 const schema = input.split(/\r\n/);
 
-const nonCoaxialSteps = (row, col, numberOfRows) => {
+const nonCoaxialSteps = (row, col, nRows) => {
   const steps = [];
   if (row - 1 >= 0) {
     steps.push({ row: row - 1, col });
   }
-  if (row + 1 <= numberOfRows - 1) {
+  if (row + 1 <= nRows - 1) {
     steps.push({ row: row + 1, col });
   }
   return steps;
@@ -37,13 +37,13 @@ const adjacentSteps = ({ row, col }, content, nRows, rowLen) => {
 let part1Solution = 0;
 for (let rowIndex = 0; rowIndex < schema.length; rowIndex += 1) {
   const row = schema[rowIndex];
-  for (const number of ints(row)) {
-    const pivot = { row: rowIndex, col: number["index"] };
-    const strNumber = number[0];
-    part1Solution += adjacentSteps(pivot, strNumber, schema.length, row.length)
+  for (const num of ints(row)) {
+    const pivot = { row: rowIndex, col: num["index"] };
+    const strNum = num[0];
+    part1Solution += adjacentSteps(pivot, strNum, schema.length, row.length)
       .reduce((sum, step) => sum + schema[step.row][step.col], "")
       .match(/[*#$@&%+/=-]/)
-      ? +strNumber
+      ? +strNum
       : 0;
   }
 }
@@ -53,22 +53,22 @@ console.log(part1Solution);
 const partNums = {};
 for (let rowIndex = 0; rowIndex < schema.length; rowIndex += 1) {
   const row = schema[rowIndex];
-  for (const number of ints(row)) {
-    const pivot = { row: rowIndex, col: number["index"] };
-    const strNumber = number[0];
-    const steps = adjacentSteps(pivot, strNumber, schema.length, row.length);
+  for (const num of ints(row)) {
+    const pivot = { row: rowIndex, col: num["index"] };
+    const strNum = num[0];
+    const steps = adjacentSteps(pivot, strNum, schema.length, row.length);
     for (const step of steps) {
       if (schema[step.row][step.col].match(/\*/)) {
-        const partNumber = partNums[`${JSON.stringify(step)}`];
-        partNums[`${JSON.stringify(step)}`] = partNumber
-          ? [...partNumber, +strNumber]
-          : [+strNumber];
+        const partNum = partNums[`${JSON.stringify(step)}`];
+        partNums[`${JSON.stringify(step)}`] = partNum
+          ? [...partNum, +strNum]
+          : [+strNum];
       }
     }
   }
 }
-const part2Solution = Object.values(partNums).reduce((sum, partNumbers) => {
-  return partNumbers[1] ? sum + partNumbers[0] * partNumbers[1] : sum;
+const part2Solution = Object.values(partNums).reduce((sum, partNums) => {
+  return partNums[1] ? sum + partNums[0] * partNums[1] : sum;
 }, 0);
 console.log(part2Solution);
 
