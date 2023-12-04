52153f4849457dee1faa1e0a98efe5827de8a222
jsuh <jsuh@hexacta.com>
2023-12-04 10:58:31 -0300

    feat: d04

---
+++ 2023/d04/solution.js
@@ -0,0 +1,43 @@
+import { range, readInput } from "../utils.js";
+
+const start = performance.now();
+const input = await readInput();
+
+const cardParts = input
+  .split(/\r\n/)
+  .map((cardInfo) => cardInfo.split(/\s\|\s/));
+const cardNums = cardParts
+  .map((parts) => parts.map((part) => part.match(/\d+\s.*/)[0]))
+  .map((numList) => numList.map((nums) => nums.split(/\s+/)));
+
+// part-1
+const part1Solution = cardNums
+  .map((cardNums) => cardNums[1].filter((num) => cardNums[0].includes(num)))
+  .map((wNums) => (wNums.length ? Math.pow(2, wNums.length - 1) : 0))
+  .reduce((sum, curr) => sum + curr, 0);
+console.log(part1Solution);
+
+// part-2
+const instances = range(1, cardParts.length, 1).reduce(
+  (cardTags, tag) => ({
+    ...cardTags,
+    [`${tag}`]: 1,
+  }),
+  {},
+);
+for (let i = 0; i < cardParts.length; i += 1) {
+  const wNumsLen = cardNums[i][1].filter((num) =>
+    cardNums[i][0].includes(num),
+  ).length;
+  const cardId = i + 1;
+  for (const aboveId of range(cardId + 1, cardId + wNumsLen, 1)) {
+    instances[`${aboveId}`] += instances[`${cardId}`];
+  }
+}
+const part2Solution = Object.values(instances).reduce(
+  (sum, curr) => sum + curr,
+  0,
+);
+console.log(part2Solution);
+
+console.log("Elapsed:", performance.now() - start);
--- 2023/utils.js
+++ 2023/utils.js
@@ -9,11 +9,6 @@ const INPUT_FILE = "input.txt";
 const FILE_PATH = join(__dirname, DAY_SUBDIR, INPUT_FILE);
 const UTF8 = "utf8";
 
-/**
- * Reads the input file in the same directory as the solution file
- * and returns a promise of the file's contents as a string
- * @returns {Promise<string>}
- */
 const readInput = () => {
   return new Promise((resolve, reject) => {
     const chunks = [];
@@ -26,24 +21,15 @@ const readInput = () => {
   });
 };
 
-/**
- * Returns a string of all keys in the objects joined by "|" (alternation)
- * @param  {...object} objects
- * @returns {string}
- */
 const alts = (...objects) =>
   objects.reduce(
     (pattern, object) => pattern + Object.keys(object).join("|"),
     "",
   );
 
-/**
- * Returns an array of all integers in the string
- * @param {string} string
- * @returns {string[]}
- */
-const ints = (string) => {
-  return [...string.matchAll(/\d+/g)];
-};
+const ints = (string) => [...string.matchAll(/\d+/g)];
 
-export { alts, ints, readInput };
+const range = (start, stop, step) =>
+  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
+
+export { alts, ints, range, readInput };
