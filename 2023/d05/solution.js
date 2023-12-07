44af0b595f0109e0f7e33ff8b2941a49306a96e7
jsuh <jsuh@hexacta.com>
2023-12-07 18:38:35 -0300

    feat: d05 part-2

--- 2023/d05/solution.js
+++ 2023/d05/solution.js
@@ -38,4 +38,81 @@ for (let seed of seeds) {
 }
 console.log(part1Solution);
 
+// part-2
+function intersect(arr1, arr2) {
+  const [low1, high1] = arr1;
+  const [low2, high2] = arr2;
+  const low = Math.max(low1, low2);
+  const high = Math.min(high1, high2);
+  if (low > high) return null;
+  return [low, high];
+}
+
+function nonIntersectingIntervals(seedInterval, intersectingIntervals) {
+  const nonIntersectingIntervals = [];
+  const [seedLow, seedHigh] = seedInterval;
+  const [firstLow] = intersectingIntervals[0];
+  if (firstLow - seedLow > 0) {
+    nonIntersectingIntervals.push([seedLow, firstLow - 1]);
+  }
+  const [, lastHigh] = intersectingIntervals[intersectingIntervals.length - 1];
+  if (seedHigh - lastHigh > 0) {
+    nonIntersectingIntervals.push([lastHigh + 1, seedHigh]);
+  }
+  for (let i = 0; i < intersectingIntervals.length - 1; i += 1) {
+    const [, currHigh] = intersectingIntervals[i];
+    const [nextLow] = intersectingIntervals[i + 1];
+    if (nextLow - currHigh - 1 > 0) {
+      nonIntersectingIntervals.push([currHigh + 1, nextLow - 1]);
+    }
+  }
+  return nonIntersectingIntervals.toSorted((int1, int2) => int1[0] - int2[0]);
+}
+
+let newSeeds = JSON.parse(JSON.stringify(seeds));
+for (const map of maps) {
+  let currMapSeeds = [];
+  for (let i = 0; i < newSeeds.length; i += 2) {
+    const intersectionIntervals = [];
+    const transformedIntersectionIntervals = [];
+    const nonIntersectionIntervals = [];
+    let seedInterval = [newSeeds[i], newSeeds[i] + newSeeds[i + 1] - 1];
+    for (const transform of map.toSorted((t1, t2) => t1[1] - t2[1])) {
+      const [destStart, intervalLow, rangeLen] = transform.map((num) => +num);
+      const intervalHigh = intervalLow + rangeLen - 1;
+      const interval = [intervalLow, intervalHigh];
+      const intersectionInterval = intersect(seedInterval, interval);
+      if (intersectionInterval) {
+        intersectionIntervals.push(intersectionInterval);
+        const transformed = [
+          intersectionInterval[0] + destStart - intervalLow,
+          intersectionInterval[1] + destStart - intervalLow,
+        ];
+        transformedIntersectionIntervals.push(transformed);
+      }
+    }
+    if (intersectionIntervals.length) {
+      nonIntersectionIntervals.push(
+        ...nonIntersectingIntervals(seedInterval, intersectionIntervals),
+      );
+    } else {
+      nonIntersectionIntervals.push(seedInterval);
+    }
+    const resultingMapIntervals = [
+      ...nonIntersectionIntervals,
+      ...transformedIntersectionIntervals,
+    ];
+    for (const interval of resultingMapIntervals) {
+      currMapSeeds.push(interval[0]);
+      currMapSeeds.push(interval[1] - interval[0] + 1);
+    }
+  }
+  newSeeds = currMapSeeds;
+}
+const part2Solution = newSeeds.reduce((acc, curr, i) => {
+  if (i % 2 === 0) return Math.min(acc, curr);
+  return acc;
+}, Infinity);
+console.log(part2Solution);
+
 console.log("Elapsed:", performance.now() - start);
