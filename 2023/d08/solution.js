566c76ae309c9da48277e04b290946d87be81bd2
jsuh <jsuh@hexacta.com>
2023-12-13 09:24:47 -0300

    feat: d08

--- 2023/d08/solution.js
+++ 2023/d08/solution.js
@@ -6,7 +6,7 @@ const input = await readInput();
 const instr = input[0].split("");
 const network = input
   .slice(2)
-  .map((nodes) => [...nodes.matchAll("[A-Z1-9]+")])
+  .map((nodes) => [...nodes.matchAll(/[A-Z]+/g)])
   .join(" ")
   .split(" ")
   .reduce((map, curr) => {
@@ -14,16 +14,14 @@ const network = input
     map.set(src, [lDest, rDest]);
     return map;
   }, new Map());
-
 const stepsTaken = (initialNodes) => {
   const stepsTaken = [];
   for (let currNode of initialNodes) {
     let instrIndex = 0;
     let steps = 0;
     while (currNode[2] !== "Z") {
-      const currInstr = instr[instrIndex];
       const destNodes = network.get(currNode);
-      currNode = currInstr === "L" ? destNodes[0] : destNodes[1];
+      currNode = instr[instrIndex] === "L" ? destNodes[0] : destNodes[1];
       instrIndex += 1;
       if (instrIndex === instr.length) instrIndex = 0;
       steps += 1;
@@ -38,12 +36,22 @@ const part1Solution = stepsTaken(["AAA"])[0];
 console.log(part1Solution);
 
 // part-2
+const gcd = (a, b) => {
+  return b === 0 ? a : gcd(b, a % b);
+};
+const lcm = (a, b) => {
+  return (a * b) / gcd(a, b);
+};
 const limitNodes = [...network.keys()]
-  .flatMap((key) => [...key.matchAll(/[A-Z1-9]+[AZ]/g)])
+  .flatMap((key) => [...key.matchAll(/[A-Z]+[AZ]/g)])
   .join(" ")
   .split(" ")
   .toSorted((a, b) => a[2].localeCompare(b[2]));
-const part2Solution = stepsTaken(limitNodes.slice(0, limitNodes.length / 2));
+const allSteps = stepsTaken(limitNodes.slice(0, limitNodes.length / 2));
+let part2Solution = allSteps.reduce(
+  (acc, curr) => lcm(acc, curr),
+  allSteps.shift(),
+);
 console.log(part2Solution);
 
 console.log("Elapsed:", performance.now() - start);
