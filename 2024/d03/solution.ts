f20ff4c37dc8dd57edcad68b290cec967e7dc16f
jlsuh <joelleonardosuh@gmail.com>
2024-12-03 12:49:36 -0300

    feat: d03

---
+++ 2024/d03/solution.ts
@@ -0,0 +1,27 @@
+import { readInput } from "../utils.js";
+
+const start = performance.now();
+const input = await readInput();
+
+const multiplyPairs = (instr: string) =>
+  (([fst, snd]) => +fst[0] * +snd[0])([...instr.matchAll(/\d+/g)]);
+
+// part-1
+const part1Solution = input[0]
+  .matchAll(/mul\(\d+,\d+\)/g)
+  .reduce((sum, [instr]) => sum + multiplyPairs(instr), 0);
+console.log(part1Solution);
+
+// part-2
+let isEnabled = true;
+const part2Solution = input[0]
+  .matchAll(/mul\(\d+,\d+\)|do\(\)|don't\(\)/g)
+  .reduce((sum, [instr]) => {
+    if (instr.match(/do\(\)/)) isEnabled = true;
+    else if (instr.match(/don't\(\)/)) isEnabled = false;
+    else if (isEnabled) return sum + multiplyPairs(instr);
+    return sum;
+  }, 0);
+console.log(part2Solution);
+
+console.log("Elapsed:", performance.now() - start);
